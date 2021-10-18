95d7b98f730170438af9d168003b5457f0894613
cody <cmchaff14@gmail.com>
2021-10-18 10:05:47 -0500

    commit tree 2fa2014b44ced631161bef60519f1c55c0d5ea95

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,20 @@
-class Dog
+# establishes the connection to the database and gives the location for the database
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/test.sqlite3"
+)
+# building the table for the database
+sql= <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+    )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+
+
+
+class Dog < ActiveRecord::Base
 end
